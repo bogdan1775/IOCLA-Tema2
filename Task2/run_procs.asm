ac9cf04fe6a3e8523b45da5a2960e97c4b5acdad
bogdan1775 <120658401+bogdan1775@users.noreply.github.com>
2023-08-09 22:27:57 +0300

    Update run_procs.asm

--- Task2/run_procs.asm
+++ Task2/run_procs.asm
@@ -4,6 +4,11 @@
     ;;   TODO: Declare 'avg' struct to match its C counterpart
     ;;
 
+struc avg
+    .quo: resw 1
+    .remain: resw 1
+endstruc
+
 struc proc
     .pid: resw 1
     .prio: resb 1
@@ -41,13 +46,94 @@ clean_results:
     ;; DO NOT MODIFY
    
     ;; Your code starts here
-
-
-
-    ;; Your code ends here
-    
-    ;; DO NOT MODIFY
-    popa
-    leave
-    ret
-    ;; DO NOT MODIFY
+	
+	
+	;; esi este contor pentru a parcurge structura proc
+	xor esi, esi
+	
+	;; se parcurge structura proc
+parcurgere_structura:
+	cmp esi, ebx
+	jge initializare
+	
+	xor edi, edi
+	mov edi, esi
+	imul edi, 5
+	xor edx, edx
+	
+	;; dl retine ce prioritate e
+	mov dl, byte [ecx + edi + 2]
+	
+	;; se adauga valoarea in vectorul prio_result
+	add dword [prio_result + edx * 4 - 4], 1
+	
+	push eax
+	xor eax, eax
+	mov ax, word [ecx + edi + 3]
+	
+	;; se adauga valoarea in vectorul time_result
+	add [time_result + edx * 4 - 4], eax
+	
+	inc esi
+	pop eax
+	jmp parcurgere_structura
+	
+	
+initializare:
+	;; esi este contor cu care parcurg cei doi vectori
+	xor esi, esi
+	
+	;; se parcurg vectorii si se initilizeaza structura avg
+punere_valori:
+	cmp esi, 5
+	jge terminare
+	
+	push eax
+	
+	xor eax, eax
+	xor edx, edx
+	xor edi, edi
+	
+	;; se retine timpul unei proritati
+	mov eax, [time_result + esi * 4]
+	
+	;; se retine numarul de prioritati cu aceeasi valoare
+	mov edi, [prio_result + esi * 4]
+	
+	;; se verifica sa nu se imparta la 0
+	cmp edi, 0
+	je verificare_dif_0
+	
+	;; se realizeaza impartirea
+	idiv edi
+	xor edi, edi
+	pop edi
+	
+	;; se retin valorile rezultate in structura
+	mov [edi + esi * 4], ax
+	mov [edi + esi * 4 + 2], dx
+	
+	inc esi
+	xor eax, eax
+	mov eax, edi
+	
+	jmp punere_valori
+	
+	
+	;; daca numarul la care se imparte e 0 se sare la urmatorul
+verificare_dif_0:
+	inc esi
+	xor eax, eax
+	pop eax
+	jmp punere_valori
+	
+	
+terminare:
+	
+	;; Your code ends here
+	
+	;; DO NOT MODIFY
+	popa
+	leave
+	ret
+	;; DO NOT MODIFY
